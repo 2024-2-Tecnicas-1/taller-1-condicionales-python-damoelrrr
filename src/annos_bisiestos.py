bfd9ed10fe8fbd712fa8fbdd3e6b7e12f7baefed
damoelrrr <danielfeliperodriguezangulo@gmail.com>
2024-09-07 22:47:50 -0500

    Update annos_bisiestos.py

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,7 +1,18 @@
 def evaluar(anno):
-    # TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-    return "";
-
+    if anno < 1582:
+        if anno % 4 == 0:
+            return f"{anno} es un año bisiesto (calendario juliano)"
+        else:
+            return f"{anno} no es bisiesto (calendario juliano)"
+    else:
+        if anno % 400 == 0:
+            return f"{anno} es un año bisiesto (calendario gregoriano)"
+        elif anno % 100 == 0:
+            return f"{anno} no es bisiesto (calendario gregoriano)"
+        elif anno % 4 == 0:
+            return f"{anno} es un año bisiesto (calendario gregoriano)"
+        else:
+            return f"{anno} no es bisiesto (calendario gregoriano)"
 if __name__ == '__main__':
     print("Año:", end="")
     anno = int(input())
