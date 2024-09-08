b8f0df42b5afe4e36eba4ba49e068ea73f68d08f
damoelrrr <danielfeliperodriguezangulo@gmail.com>
2024-09-07 23:08:27 -0500

    Update set_de_tenis.py

--- src/set_de_tenis.py
+++ src/set_de_tenis.py
@@ -1,12 +1,11 @@
 def evaluar(num_victorias_a, num_victorias_b):
     # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
-
-if __name__ == '__main__':
-    print("Los juegos ganaddor por A:", end="")
-    num_victorias_a = int(input())
-    print("Los juegos ganaddor por B:", end="")
-    num_victorias_b = int(input())
-
-    respuesta = evaluar(num_victorias_a, num_victorias_b)
-    print(respuesta)
+    if (num_victorias_a == 3 and num_victorias_b == 7) or (num_victorias_b == 7 and num_victorias_a == 3) or \
+       (num_victorias_a == 0 and num_victorias_b == 7) or (num_victorias_b == 0 and num_victorias_a == 7) or \
+       (num_victorias_a == 8 or num_victorias_b == 8) or (num_victorias_a > 7 or num_victorias_b > 7):
+        return "Inválido"
+    if (num_victorias_a >= 6 and num_victorias_a - num_victorias_b >= 2) or (num_victorias_a == 7 and num_victorias_b == 6):
+        return "Ganó A"
+    if (num_victorias_b >= 6 and num_victorias_b - num_victorias_a >= 2) or (num_victorias_a == 6 and num_victorias_b == 7):
+        return "Ganó B"
+    return "Aún no termina"
